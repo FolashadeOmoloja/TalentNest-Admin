738c743830a18b1c1bc37517488b76e89075e55e
FolashadeOmoloja <omolojashade@gmail.com>
2025-05-07 00:09:38 +0100

    middleware and progressbar

--- utilities/constants/constants.ts
+++ utilities/constants/constants.ts
@@ -1,5 +1,9 @@
-export const ADMIN_API_END_POINT = "http://localhost:8000/api/v1/admin";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const BLOG_API_END_POINT = "http://localhost:8000/api/v1/blog";
-export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/applicants";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api/v1"
+    : "https://talentnest-backend.onrender.com/api/v1";
+
+export const ADMIN_API_END_POINT = `${BASE_URL}/admin`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const BLOG_API_END_POINT = `${BASE_URL}/blog`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/applicants`;
